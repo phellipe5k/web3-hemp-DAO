e77ea71c8dbcb3f1d0bde23dd90e6fd88a8d499b
Luiz Andrade <luizinhophe@hotmail.com>
2022-04-16 23:48:21 -0300

    fisrt commit - hemp dao

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,5 @@
 import * as S from './style';
-
-// Icons
-import { NextDotJs as NextJsIcon } from '@styled-icons/simple-icons';
+import { Button } from 'web3-components';
 
 type Props = {
   title?: string;
@@ -11,10 +9,18 @@ type Props = {
 
 const Main = ({ title, description, illustrationSrc }: Props) => (
   <S.Container>
-    <NextJsIcon color="white" />
-    <S.Title>{title}</S.Title>
-    <S.Description>{description}</S.Description>
-    <S.Illustration src={illustrationSrc} />
+    <S.Hero>
+      <S.Title>Welcome to Hemp DAO </S.Title>
+      <S.Description>Here you can earn a little <span style={{ color:'#1E7C51' }}>GREEN</span> NFT being a Hemp DAO membership.</S.Description>
+      <S.WrapperButtons>
+        {/*@ts-ignore */}
+      <Button.Transparent width='40%' style={{ borderRadius: '6px' }} glow={'#1E7C51'} border="none" color='#1E7C51'>Connect my wallet</Button.Transparent>
+        {/*@ts-ignore */}
+      <Button.Default width='40%' style={{ borderRadius: '6px' }} color='transparent' textColor='#1E7C51' border='#1E7C51'>Get Metamask</Button.Default>
+      </S.WrapperButtons>
+    
+    </S.Hero>
+    <S.Hemp src='/assets/hemp.png' />
   </S.Container>
 );
 
--- src/components/Main/style.ts
+++ src/components/Main/style.ts
@@ -1,16 +1,17 @@
 import styled from 'styled-components';
 
 export const Container = styled.main`
-  background-color: #345df6;
-  color: #fff;
+  background-color: whitesmoke;
+  color: #002E35;
   width: 100%;
   height: 100%;
   display: flex;
-  flex-direction: column;
   padding: 3%;
-  text-align: center;
-  justify-content: center;
+  justify-content: flex-end;
   align-items: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url('/assets/background.png');
 `;
 
 export const Logo = styled.img`
@@ -18,14 +19,18 @@ export const Logo = styled.img`
 `;
 
 export const Title = styled.h1`
-  font-size: 4rem;
-  padding: 2% 0;
+  font-size: 9rem;
+  width: 80%;
+  line-height: 110px;
 `;
 
 export const Description = styled.p`
   font-size: 2rem;
   padding: 2% 0;
+  color:#8A939A;
   width: 60%;
+  margin: 16px 0;
+  font-weight: 500;
 `;
 
 export const Illustration = styled.img`
@@ -33,4 +38,28 @@ export const Illustration = styled.img`
   background-color: white;
   padding: 0.5%;
   border-radius: 15px;
+`;
+
+export const Hero = styled.div`
+  padding: 16px;
+  display: flex;
+  width: 48%;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const Hemp = styled.img`
+  transform:  rotate(90deg);
+  filter: drop-shadow(20px -10px 20px #242424);
+  -webkit-filter: drop-shadow(40px -25px 30px #242424);
+
+`;
+
+export const WrapperButtons = styled.div`
+  width: 70%;
+  justify-content: space-around;
+  display: flex;
+  button:nth-child(2) {
+    font-weight:  800;
+  }
 `;
