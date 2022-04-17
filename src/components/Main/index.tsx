7f53738c8b5a24f80684c474e301c367493fd2b9
Luiz Andrade <luizinhophe@hotmail.com>
2022-04-17 03:23:46 -0300

    inserting third web on the project

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,6 @@
 import * as S from './style';
 import { Button } from 'web3-components';
+import { useAddress, useMetamask } from '@thirdweb-dev/react';
 
 type Props = {
   title?: string;
@@ -7,21 +8,32 @@ type Props = {
   illustrationSrc?: string;
 };
 
-const Main = ({ title, description, illustrationSrc }: Props) => (
-  <S.Container>
-    <S.Hero>
-      <S.Title>Welcome to Hemp DAO </S.Title>
-      <S.Description>Here you can earn a little <span style={{ color:'#1E7C51' }}>GREEN</span> NFT being a Hemp DAO membership.</S.Description>
-      <S.WrapperButtons>
-        {/*@ts-ignore */}
-      <Button.Transparent width='40%' style={{ borderRadius: '6px' }} glow={'#1E7C51'} border="none" color='#1E7C51'>Connect my wallet</Button.Transparent>
-        {/*@ts-ignore */}
-      <Button.Default width='40%' style={{ borderRadius: '6px' }} color='transparent' textColor='#1E7C51' border='#1E7C51'>Get Metamask</Button.Default>
-      </S.WrapperButtons>
-    
-    </S.Hero>
-    <S.Hemp src='/assets/hemp.png' />
-  </S.Container>
-);
+const Main = ({ title, description, illustrationSrc }: Props) => {
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  return (
+    <S.Container>
+      { !address ? (
+        <>
+        <S.Hero>
+          <S.Title>Welcome to Hemp DAO </S.Title>
+          <S.Description>Here you can earn a little <span style={{ color:'#1E7C51' }}>GREEN</span> NFT being a Hemp DAO membership.</S.Description>
+          <S.WrapperButtons>
+            {/*@ts-ignore */}
+          <Button.Transparent onClick={ connectWithMetamask } width='40%' style={{ borderRadius: '6px' }} glow={'#1E7C51'} border="none" color='#1E7C51'>Connect my wallet</Button.Transparent>
+            {/*@ts-ignore */}
+          <Button.Default onClick={() => window.redirect('https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn')} width='40%' style={{ borderRadius: '6px' }} color='transparent' textColor='#1E7C51' border='#1E7C51'>Get Metamask</Button.Default>
+          </S.WrapperButtons>
+        
+        </S.Hero>
+        <S.Hemp src='/assets/hemp.png' />
+      </>
+      ) 
+      : <S.Hero>
+          <S.Title>Conected</S.Title>
+        </S.Hero>}
+    </S.Container>
+  );
+}
 
 export default Main;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,30 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import GlobalStyles from 'styles/global';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+
 
 const myApp = ({ Component, pageProps }: AppProps) => {
+  
+  const activeChainId = ChainId.Rinkeby
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>Hemp DAO</title>
         <link
           rel="shortcut icon"
-          href="https://reactavancado.com.br/img/icon-512.png"
+          href="assets/weed.png"
         />
         <link
           rel="apple-touch-icon"
-          href="https://reactavancado.com.br/img/icon-512.png"
+          href="assets/weed.png"
         />
         <link rel="manifest" href="/manifest.json" />
       </Head>
       <GlobalStyles />
-      <Component {...pageProps} />
+      <ThirdwebProvider desiredChainId={activeChainId}>
+        <Component {...pageProps} />
+      </ThirdwebProvider>
     </>
   );
 };
